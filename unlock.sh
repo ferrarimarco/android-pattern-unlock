c90238065ce71651517f529787bc0e8d5c01d56c
Marco Ferrari <ferrari.marco@gmail.com>
2017-10-18 16:44:18 +0200

    Adapted values for the Nexus 4 screen

--- unlock.sh
+++ unlock.sh
@@ -33,16 +33,13 @@
 
 PATTERN="1 2 3 6 5 4 7 8 9" # The unlock pattern to draw, space seperated
 
-COL_1=166                   # X coordinate of column 1 (in pixels)
-COL_2=384                   # X coordinate of column 2 (in pixels)
-COL_3=576                   # X coordinate of column 3 (in pixels)
+COL_1=391                   # X coordinate of column 1 (in pixels)
+COL_2=778                   # X coordinate of column 2 (in pixels)
+COL_3=1165                   # X coordinate of column 3 (in pixels)
 
-ROW_1=553                   # Y coordinate of row 1 (in pixels)
-ROW_2=738                   # Y coordinate of row 2 (in pixels)
-ROW_3=984                   # Y coordinate of row 3 (in pixels)
-
-MULTIPLIER=2                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
-                            # Samsung Galaxy S2, set this to 1. Experiment with this value if you can't see anything happening.
+ROW_1=1155                   # Y coordinate of row 1 (in pixels)
+ROW_2=1627                   # Y coordinate of row 2 (in pixels)
+ROW_3=1969                   # Y coordinate of row 3 (in pixels)
 
 WAKE_SCREEN_ENABLED=true    # If true, the script will start by sending the power button press event
 
@@ -55,25 +52,25 @@ SWIPE_UP_Y_TO=200           # End Y coordinate for initial upward swipe. Only us
 
 # Define X&Y coordinates for each of the 9 positions.
 
-X[1]=$(( ${COL_1} * ${MULTIPLIER} ))
-X[2]=$(( ${COL_2} * ${MULTIPLIER} ))
-X[3]=$(( ${COL_3} * ${MULTIPLIER} ))
-X[4]=$(( ${COL_1} * ${MULTIPLIER} ))
-X[5]=$(( ${COL_2} * ${MULTIPLIER} ))
-X[6]=$(( ${COL_3} * ${MULTIPLIER} ))
-X[7]=$(( ${COL_1} * ${MULTIPLIER} ))
-X[8]=$(( ${COL_2} * ${MULTIPLIER} ))
-X[9]=$(( ${COL_3} * ${MULTIPLIER} ))
+X[1]=$(( ${COL_1} ))
+X[2]=$(( ${COL_2} ))
+X[3]=$(( ${COL_3} ))
+X[4]=$(( ${COL_1} ))
+X[5]=$(( ${COL_2} ))
+X[6]=$(( ${COL_3} ))
+X[7]=$(( ${COL_1} ))
+X[8]=$(( ${COL_2} ))
+X[9]=$(( ${COL_3} ))
 
-Y[1]=$(( ${ROW_1} * ${MULTIPLIER} ))
-Y[2]=$(( ${ROW_1} * ${MULTIPLIER} ))
-Y[3]=$(( ${ROW_1} * ${MULTIPLIER} ))
-Y[4]=$(( ${ROW_2} * ${MULTIPLIER} ))
-Y[5]=$(( ${ROW_2} * ${MULTIPLIER} ))
-Y[6]=$(( ${ROW_2} * ${MULTIPLIER} ))
-Y[7]=$(( ${ROW_3} * ${MULTIPLIER} ))
-Y[8]=$(( ${ROW_3} * ${MULTIPLIER} ))
-Y[9]=$(( ${ROW_3} * ${MULTIPLIER} ))
+Y[1]=$(( ${ROW_1} ))
+Y[2]=$(( ${ROW_1} ))
+Y[3]=$(( ${ROW_1} ))
+Y[4]=$(( ${ROW_2} ))
+Y[5]=$(( ${ROW_2} ))
+Y[6]=$(( ${ROW_2} ))
+Y[7]=$(( ${ROW_3} ))
+Y[8]=$(( ${ROW_3} ))
+Y[9]=$(( ${ROW_3} ))
 
 # Function definitions
 
